ae885940e34a07883c736fa83a1058758cfd0260
Ivan <ivan_lape@live.com>
2020-06-13 14:34:00 +0200

    final version
    Финална верзија

---
+++ src/test/java/SILab2Test.java
@@ -0,0 +1,42 @@
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class SILab2Test {
+
+    @Test
+    public void functionEveryPath() {
+        assertEquals(SILab2.function(List.of("0","0","0")),List.of("0","0","0"));
+        assertEquals(SILab2.function(List.of("0","0","#")),List.of("0","1","#"));
+        assertEquals(SILab2.function(List.of("0","#","0")),List.of("1","#","1"));
+        assertEquals(SILab2.function(List.of("0","#","#")),List.of("1","#","#"));
+        assertEquals(SILab2.function(List.of("#","0","0")),List.of("#","1","0"));
+        assertEquals(SILab2.function(List.of("#","0","#")),List.of("#","2","#"));
+        assertEquals(SILab2.function(List.of("#","#","0")),List.of("#","#","1"));
+        assertEquals(SILab2.function(List.of("#")),List.of("#"));
+        assertEquals(SILab2.function(List.of("0")),List.of("0"));
+        assertEquals(SILab2.function(List.of("0","0")),List.of("0","0"));
+        assertEquals(SILab2.function(List.of("0","#")),List.of("1","#"));
+        assertEquals(SILab2.function(List.of("#","0")),List.of("#","1"));
+        assertEquals(SILab2.function(List.of("#","#")),List.of("#","#"));
+    }
+    @Test
+    public void functionEveryStatement()
+    {
+        assertEquals(SILab2.function(List.of("0","0","0")),List.of("0","0","0"));
+        assertEquals(SILab2.function(List.of("0","0","#")),List.of("0","1","#"));
+        assertEquals(SILab2.function(List.of("0","#","0")),List.of("1","#","1"));
+        assertEquals(SILab2.function(List.of("0","#","#")),List.of("1","#","#"));
+        assertEquals(SILab2.function(List.of("#","0","0")),List.of("#","1","0"));
+        assertEquals(SILab2.function(List.of("#","0","#")),List.of("#","2","#"));
+        assertEquals(SILab2.function(List.of("#","#","0")),List.of("#","#","1"));
+        assertEquals(SILab2.function(List.of("#")),List.of("#"));
+        assertEquals(SILab2.function(List.of("0")),List.of("0"));
+        assertEquals(SILab2.function(List.of("0","0")),List.of("0","0"));
+        assertEquals(SILab2.function(List.of("0","#")),List.of("1","#"));
+        assertEquals(SILab2.function(List.of("#","0")),List.of("#","1"));
+        assertEquals(SILab2.function(List.of("#","#")),List.of("#","#"));
+    }
+}
